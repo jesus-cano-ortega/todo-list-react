83e6c1a033d5e1fc884d73f600d30b6514f9ceb6
Jesús Cano <jcanort@gmail.com>
2021-11-26 19:36:13 +0000

    Adding new task to form

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	//Hooks
+	const [value, setValue] = useState("");
+	const [items, setItems] = useState([]);
+	const [counter, setCounter] = useState(items.length);
+
+	//Function that creates a new item inside the array when press Enter
+	const createNewTask = ev => {
+		if (ev.key == "Enter" && value != "") {
+			setItems(items => [...items, { name: value }]);
+			setCounter(counter + 1);
+			setValue("");
+		}
+	};
+
+	//Function that deletes an element inside the array when press button
+	const deleteTask = index => {
+		let newArray = [...items];
+		newArray.splice(index, 1);
+		setItems(newArray);
+		setCounter(counter - 1);
+	};
+
+	//Function of the task structure
+	const taskList = items.map((item, index) => {
+		return (
+			<>
+				<div key={index}>
+					<li className="text-white">{item.name}</li>
+					<button onClick={deleteTask}>Delete Task</button>
+				</div>
+			</>
+		);
+	});
+
+	//Function that shows the number of tasks
+	const pendingTask = () => {
+		if (counter == 0) return "No task";
+		else return counter;
+	};
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="container-fluid body d-flex align-items-center justify-content-center">
+				<div className="container content">
+					<div className="container d-flex flex-column align-items-center text-white">
+						<h1>Friday</h1>
+						<p>26/11/2021</p>
+						<p>{pendingTask()}</p>
+					</div>
+					<input
+						type="text"
+						placeholder="Add a task..."
+						onChange={ev => {
+							setValue(ev.target.value);
+						}}
+						onKeyPress={ev => {
+							createNewTask(ev);
+						}}
+						value={value}
+					/>
+					<ul>{taskList}</ul>
+				</div>
+			</div>
+		</>
 	);
 };
 
