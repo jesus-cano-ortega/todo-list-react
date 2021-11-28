9ed34bee6b2df10555cf6a985bb3cfd6b2cb8d11
Jesús Cano <jcanort@gmail.com>
2021-11-28 18:34:44 +0000

    Adding styles to input and list item button

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -26,41 +26,43 @@ const Home = () => {
 
 	//Function that shows the number of tasks
 	const pendingTask = () => {
-		if (counter == 0) return "No task";
-		else return counter;
+		if (counter == 0) return "No pending tasks";
+		else return `You have ${counter} pending tasks`;
 	};
 
 	return (
 		<>
 			<div className="container-fluid body d-flex align-items-center justify-content-center">
 				<div className="container content">
-					<div className="container d-flex flex-column align-items-center text-white">
-						<h1>Friday</h1>
-						<p>26/11/2021</p>
+					<div className="container d-flex flex-column align-items-center text-white py-3 px-2">
+						<h1>ToDo List</h1>
 						<p>{pendingTask()}</p>
 					</div>
-					<input
-						type="text"
-						placeholder="Add a task..."
-						onChange={ev => {
-							setValue(ev.target.value);
-						}}
-						onKeyPress={ev => {
-							createNewTask(ev);
-						}}
-						value={value}
-					/>
-					<ul>
-						{items.map((item, index) => {
-							return (
-								<Item
-									name={item.name}
-									key={index}
-									click={deleteTask}
-								/>
-							);
-						})}
-					</ul>
+					<div className="container d-flex flex-column justify-content-center">
+						<input
+							type="text"
+							className="form-control"
+							placeholder=" Add a new task..."
+							onChange={ev => {
+								setValue(ev.target.value);
+							}}
+							onKeyPress={ev => {
+								createNewTask(ev);
+							}}
+							value={value}
+						/>
+						<ul>
+							{items.map((item, index) => {
+								return (
+									<Item
+										name={item.name}
+										key={index}
+										click={deleteTask}
+									/>
+								);
+							})}
+						</ul>
+					</div>
 				</div>
 			</div>
 		</>
--- src/js/component/item.jsx
+++ src/js/component/item.jsx
@@ -4,8 +4,12 @@ import PropTypes from "prop-types";
 const Item = props => {
 	return (
 		<>
-			<li className="text-white">{props.name}</li>
-			<button onClick={props.click}>Delete task</button>
+			<div className="container d-flex flex-row align-items-center pt-4 justify-content-between">
+				<li className="text-white">{props.name}</li>
+				<button className="btn btn-outline-light" onClick={props.click}>
+					X
+				</button>
+			</div>
 		</>
 	);
 };
